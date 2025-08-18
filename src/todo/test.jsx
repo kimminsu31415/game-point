7108c87a0082214c81fd3db1f1796eae24ec1ddd
minsu kim <kimminsu31415@gmail.com>
2025-08-18 22:09:43 +0900

    add: github ui 컴포넌트

--- src/todo/test.jsx
+++ src/todo/test.jsx
@@ -5,3 +5,11 @@ import { Button } from '@/components/ui/button';
 export default function Test() {
   return <Button>Click me</Button>;
 }
+
+// github ui 컴포넌트 라이브러리 사용하기
+
+import { Button } from '@/components/ui/button';
+
+export default function Test() {
+  return <Button>Click me</Button>;
+}
